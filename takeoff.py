2a67e30a8da635c9cad54eb711736bf9587403e4
Ian Riley <iandreariley@gmail.com>
2017-04-21 13:26:46 -0700

    Add function to compute heading from target position.

--- takeoff.py
+++ takeoff.py
@@ -1,6 +1,8 @@
 from dronekit import *
 import time
 import math
+from math import cos, tan, sin
+import numpy as np
 
 vehicle = connect('udpin:0.0.0.0:14551', wait_ready=True)
 messages = vehicle.message_factory
@@ -110,6 +112,37 @@ def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
     targetLocation = get_location_metres(currentLocation, dNorth, dEast)
     targetDistance = get_distance_metres(currentLocation, targetLocation)
     gotoFunction(targetLocation)
+
+def condition_yaw(heading, relative=False):
+    """
+    Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+    This method sets an absolute heading by default, but you can set the `relative` parameter
+    to `True` to set yaw relative to the current yaw heading.
+
+    By default the yaw of the vehicle will follow the direction of travel. After setting 
+    the yaw using this function there is no way to return to the default yaw "follow direction 
+    of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+    For more information see: 
+    http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+    """
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
         
 def constant_update():
     print "starting update"
@@ -117,17 +150,66 @@ def constant_update():
     counter = 0
     while True:
         val = float(counter) * math.pi / 1000.0
-        n = magnitude * math.cos(val)
+        cos_val = math.cos(val)
+        n = magnitude * cos_val
         e = magnitude * math.sin(val)
-        if counter % 10 == 0:
-            print "n: %g; e: %g" % (n,e)
+        heading = val * 180.0 / math.pi
+        print "n: %g; e: %g, heading: %g" % (n,e,heading)
         set_velocity(n, e)
-        time.sleep(0.01)
+        condition_yaw(heading)
+        time.sleep(0.1)
         counter += 1
 
 def get_position_input(position_input):
     return map(int, position_input.split())
 
+PIX_WIDTH = 960
+PIX_HEIGHT = 720
+CAM_X_ANGLE = 48.5 * 180 / math.pi
+CAM_Y_ANGLE = 28.0 * 180 / math.pi
+
+def set_velocity_from_image(img_x, img_y):
+    yaw = vehicle.attitude.yaw
+    pitch = vehicle.attitude.pitch
+    roll = vehicle.attitude.roll
+    altitude = vehicle.location.global_relative_frame.alt
+    alpha = -pitch
+
+    # TODO: Make sure x_real isn't an int
+    x_real = img_x / PIX_WIDTH / 2.0
+    y_real = float(img_y) / float(PIX_HEIGHT) / 2.0
+    theta_x = CAM_X_ANGLE * x_real
+    theta_y = CAM_Y_ANGLE * y_real
+
+    y_rel = altitude / (math.tan(pitch) - theta_y)
+    x_rel = y_rel * math.tan(theta_x)
+
+    rbn_roll = np.array([
+        [1,0,0],
+        [0, cos(roll), sin(roll)],
+        [0, -sin(roll), cos(roll)]
+    ])
+
+    rbn_yaw = np.array([
+        [cos(yaw), sin(yaw), 0],
+        [-sin(yaw), cos(yaw), 0],
+        [0, 0, 1]
+    ])
+
+    rbn_pitch = np.array([
+        [cos(pitch), 0, -sin(pitch)],
+        [0, 1, 0],
+        [sin(pitch), 0, cos(pitch)]
+    ])
+
+    position = np.array([x_rel, y_rel, -altitude])
+
+    n, e, d = rbn_roll.dot(rbn_yaw.dot(position))
+    horz_plane = np.array([n,e])
+    unit_vector = 15.64 * horz_plane / np.linalg.norm(horz_plane)
+
+    set_velocity(unit_vector[0], unit_vector[1])
+    
 def set_velocity(n, e):
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
         0,       # time_boot_ms (not used)
