780370e516564eb4135a14cf9ab627d86e642786
Ian Riley <iandreariley@gmail.com>
2017-04-21 16:13:32 -0700

    Clean up code a bit so it's not so cluttered.

--- control_test.py
+++ control_test.py
@@ -14,10 +14,6 @@ CAM_Y_ANGLE = 28.0 * 180 / math.pi
 vehicle = connect('udpin:0.0.0.0:14551', wait_ready=True)
 messages = vehicle.message_factory
 
-def get_input():
-    print '<<<',
-    return raw_input().strip()
-
 def arm_and_takeoff(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
@@ -53,73 +49,6 @@ def arm_and_takeoff(aTargetAltitude):
         time.sleep(1)
 
 
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
-
-    The function is useful when you want to move the vehicle around specifying locations relative to 
-    the current vehicle position.
-
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-        
-    return targetlocation;
-
-
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
-
-    This method is an approximation, and will not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-
-def get_bearing(aLocation1, aLocation2):
-    """
-    Returns the bearing between the two LocationGlobal objects passed as parameters.
-
-    This method is an approximation, and may not be accurate over large distances and close to the 
-    earth's poles. It comes from the ArduPilot test code: 
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """	
-    off_x = aLocation2.lon - aLocation1.lon
-    off_y = aLocation2.lat - aLocation1.lat
-    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
-    if bearing < 0:
-        bearing += 360.00
-    return bearing;
-
-
-def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):    
-    currentLocation = vehicle.location.global_relative_frame
-    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
-    targetDistance = get_distance_metres(currentLocation, targetLocation)
-    gotoFunction(targetLocation)
-
 def condition_yaw(heading, relative=False):
     """
     Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
@@ -150,25 +79,6 @@ def condition_yaw(heading, relative=False):
         0, 0, 0)    # param 5 ~ 7 not used
     # send command to vehicle
     vehicle.send_mavlink(msg)
-        
-def constant_update():
-    print "starting update"
-    counter = 0
-
-    # This should represent the middle of the camera, more or less.
-    # so the heading shouldn't curve at all
-    x = PIX_WIDTH / 2.0
-    y = PIX_HEIGHT / 2.0
-    while True:
-        if counter % 100 == 0:
-            set_velocity_from_image(x,y)
-        else:
-            set_velocity_from_image(x,y,True)
-        time.sleep(0.1)
-        counter += 1
-
-def get_position_input(position_input):
-    return map(int, position_input.split())
 
 def set_velocity_from_image(img_x, img_y, debug=False):
     yaw = vehicle.attitude.yaw
@@ -250,9 +160,32 @@ def set_position(north, east, down):
         0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
     vehicle.send_mavlink(msg)
 
+def get_input():
+    print '<<<',
+    return raw_input().strip()
+
+def get_position_input(position_input):
+    return map(int, position_input.split())
+            
+def constant_update():
+    print "starting update"
+    counter = 0
+
+    # This should represent the middle of the camera, more or less.
+    # so the heading shouldn't curve at all
+    x = PIX_WIDTH / 2.0
+    y = PIX_HEIGHT / 2.0
+    while True:
+        if counter % 100 == 0:
+            set_velocity_from_image(x,y)
+        else:
+            set_velocity_from_image(x,y,True)
+        time.sleep(0.1)
+        counter += 1
+
 def repl_loop():
     
-    print "\nEnter any code you'd like to execute. the vehicle object is called 'vehicle'; so the following will print the vehicle's attitude: 'print vehicle.attitude'"
+    print "\nEnter image x and y coordinates separated by a space and see what happens!"
 
     command = get_input()
     print "Executing command: %s" % command
