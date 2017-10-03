a1b53ed38ab278824003742a5110ba16517682d6
Ian Riley <iandreariley@gmail.com>
2017-10-03 08:54:24 -0700

    Clean up TrackingViewer and Tracking Results code.

--- evaluation.py
+++ evaluation.py
@@ -6,24 +6,43 @@ import numpy as np
 class TrackingViewer:
     """Tracker subscriber that provides a viewing GUI for the tracking process"""
 
+    BBOX_COLOR = (255, 0, 0)
+
     def __init__(self, title="view"):
         self._title = title
 
     def update(self, tracker):
+        """Update viewer to show most recent image and bounding box.
+
+        Args:
+            tracker (tracker.SimpleTracker): Object that is running the tracking session.
+
+        Returns:
+            None
+        """
         tl, br = util.to_tl_br(tracker.current_location)
-        image = np.copy(tracker.current_image)
         tl = tuple(map(int, tl))
         br = tuple(map(int, br))
 
-        tr = (br[0], tl[1])
-        bl = (tl[0], br[1])
-        cv2.line(image, tl, tr, (255, 0, 0), 4)
-        cv2.line(image, tr, br, (255, 0, 0), 4)
-        cv2.line(image, br, bl, (255, 0, 0), 4)
-        cv2.line(image, bl, tl, (255, 0, 0), 4)
+        bbox_image = self._draw_bounding_box(tracker.current_image, tl, br)
+        cv2.imshow(self._title, bbox_image)
+        cv2.waitKey(10)
 
-        cv2.imshow(self._title, image)
-        k = cv2.waitKey(10)
+    def _draw_bounding_box(self, original_image, top_left, bottom_right):
+        # draw on copy of image, not original.
+        image = np.copy(original_image)
+
+        # get the top-right and bottom-left corners
+        top_right = (bottom_right[0], top_left[1])
+        bottom_left = (top_left[0], bottom_right[1])
+
+        # draw bounding box.
+        cv2.line(image, top_left, top_right, self.BBOX_COLOR, 4)
+        cv2.line(image, top_right, bottom_right, self.BBOX_COLOR, 4)
+        cv2.line(image, bottom_right, bottom_left, self.BBOX_COLOR, 4)
+        cv2.line(image, bottom_left, top_left, self.BBOX_COLOR, 4)
+
+        return image
 
 
 class TrackingResults:
@@ -37,7 +56,7 @@ class TrackingResults:
         elapsed_time (int): Time elapsed during tracking session.
     """
 
-    def __init__(self, predictions, first_bbox=None, time=None, ground_truth=None):
+    def __init__(self, predictions, first_bbox=None, elapsed_time=None, ground_truth=None):
         self.first_bbox = first_bbox
         self.predictions = predictions
         self.ground_truth = ground_truth
