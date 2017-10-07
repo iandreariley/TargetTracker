8a9ce2ba5d780f8ed56dd1143437c1c095c0ab03
Ian Riley <iandreariley@gmail.com>
2017-10-06 17:16:59 -0700

    Add ground truth box to run_results.

--- run_results.py
+++ run_results.py
@@ -5,24 +5,29 @@ import argparse
 import pickle
 
 
+PRED_COLOR = (0, 0, 255)
+GT_COLOR = (255, 0, 0)
+
+
 def get_args():
     parser = argparse.ArgumentParser(description="Visualize results of previous tracking session.")
     parser.add_argument("results_file", help="Picked TrackingResults object that contains the tracking results.")
     parser.add_argument("--video_dir", help="Video dir if different than stored", default="")
+    parser.add_argument("--show_gt", action="store_true", help="Show ground truth bounding box.")
     return parser.parse_args()
 
 
-def draw_bbox(image, bbox):
+def draw_bbox(image, bbox, color):
     top_left = bbox[0], bbox[1]
     bottom_right = bbox[0] + bbox[2], bbox[1] + bbox[3]
     top_right = (bottom_right[0], top_left[1])
     bottom_left = (top_left[0], bottom_right[1])
 
     # draw bounding box.
-    cv2.line(image, top_left, top_right, (255, 0, 0), 4)
-    cv2.line(image, top_right, bottom_right, (255, 0, 0), 4)
-    cv2.line(image, bottom_right, bottom_left, (255, 0, 0), 4)
-    cv2.line(image, bottom_left, top_left, (255, 0, 0), 4)
+    cv2.line(image, top_left, top_right, color, 4)
+    cv2.line(image, top_right, bottom_right, color, 4)
+    cv2.line(image, bottom_right, bottom_left, color, 4)
+    cv2.line(image, bottom_left, top_left, color, 4)
 
 
 def resolve_directory(image_file, args):
@@ -38,10 +43,13 @@ def main(args):
     with open(args.results_file, 'rb') as results_file:
         results = pickle.load(results_file)
 
-    for image_file, bbox in results.predictions.items():
+    for i, item in enumerate(results.predictions.items()):
+        image_file, bbox = item
         image_file = resolve_directory(image_file, args)
         image = cv2.imread(image_file)
-        draw_bbox(image, bbox)
+        if args.show_gt:
+            draw_bbox(image, map(int, results.ground_truth[i]), GT_COLOR)
+        draw_bbox(image, bbox, PRED_COLOR)
         cv2.imshow('preview', image)
         cv2.waitKey(10)
 
