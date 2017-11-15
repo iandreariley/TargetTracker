3c62543f8366826642e695e03e969625beddb9ff
Ian Riley <iandreariley@gmail.com>
2017-11-15 13:56:44 -0800

    Fix find centroid method.

--- run_old.py
+++ run_old.py
@@ -19,8 +19,8 @@ video_directory = "videos/"
 BAUD = 921600
 
 def get_centroid(CMT):
-        x = CMT.tl[0] - CMT.tr[0]
-        y = CMT.tl[1] - CMT.br[1]
+        x = (CMT.tl[0] + CMT.tr[0]) / 2
+        y = (CMT.tl[1] + CMT.br[1]) / 2
         return np.array([x,y])
 
 def create_video_directory():
@@ -32,7 +32,42 @@ def create_video_directory():
         os.mkdir(new_video_path)
         return new_video_path
 
-        
+def arm_and_takeoff(vehicle, aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto
+    #  (otherwise the command after Vehicle.simple_takeoff will execute
+    #   immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        # Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+
 CMT = CMT.CMT()
 
 parser = argparse.ArgumentParser(description='Track an object.')
@@ -48,12 +83,13 @@ CMT.estimate_scale = True
 CMT.estimate_rotation = False
 print(args.vehicleurl)
 try:
-        vehicle = connect(args.vehicleurl, baud=BAUD, wait_ready=False)
-        vehicle.mode = VehicleMode('GUIDED')
-        vehicle.armed = True
+        vehicle = connect(args.vehicleurl, baud=BAUD, wait_ready=True)
+        arm_and_takeoff(vehicle, 10)
 except Exception as inst:
         print inst.args
-        print 'Could not connect to vehicle, exiting.'
+        print 'Could not connect to vehicle, landing and exiting.'
+        vehicle.mode = VehicleMode("RTL")
+        vehicle.close()
         sys.exit(1)
 
  # Clean up
@@ -69,7 +105,7 @@ while not cap.isOpened() and i < 10:
         i += 1
 
 print("connected on %d" % (i - 1))
-        
+
 if preview is None:
         preview = True
 
@@ -132,15 +168,15 @@ while not stopped:
                 break
         im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         im_draw = np.copy(im)
-        print(im.shape)
         CMT.process_frame(im_gray)
 
         # Display results
         if CMT.has_result:
                 # get centroid of bounding box and update drone velocity vectors.
                 center_x,center_y = get_centroid(CMT)
+                print "x: {0}\ny:{1}\n\n".format(center_x, center_y)
                 set_velocity_from_image(vehicle, center_x, center_y)
-                
+
                 # Draw updated estimate
                 cv2.line(im_draw, CMT.tl, CMT.tr, (255, 0, 0), 4)
                 cv2.line(im_draw, CMT.tr, CMT.br, (255, 0, 0), 4)
